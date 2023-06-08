52a202b220eb8a0b10e0a0f3f52c7abfa89d53b8
sweetSD <an060875@naver.com>
2023-06-08 17:56:02 +0900

    UDP 통신 방식 추가

--- GyuNet/Program.cs
+++ GyuNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,49 +9,204 @@ using System.Threading.Tasks;
 
 namespace GyuNet
 {
+    namespace Unity
+    {
+        class Room
+        {
+            public List<Session> Sessions = new List<Session>();
+        }
+
+        class UnityGyuNet
+        {
+            private TcpGyuNet tcpGyuNet = new TcpGyuNet();
+
+            private SortedSet<int> networkIDs = new SortedSet<int>();
+            private int networkID = 0;
+
+            private ConcurrentBag<Room> Rooms { get; } = new ConcurrentBag<Room>();
+            private ConcurrentDictionary<int, Room> SessionRoomPair { get; } = new ConcurrentDictionary<int, Room>();
+
+            public bool IsRunning => tcpGyuNet.IsRunning;
+
+            public void Start()
+            {
+                tcpGyuNet.OnAccepted += OnAccepted;
+                tcpGyuNet.OnReceivedPacket += OnReceivePacket;
+                tcpGyuNet.OnDisconnected += OnDisconnect;
+
+                tcpGyuNet.Start();
+            }
+
+            public void Stop()
+            {
+                tcpGyuNet.Stop();
+            }
+
+            void OnAccepted(GyuNet net, Session session)
+            {
+                if (session is TCPSession tcpSession)
+                    Debug.Log($"TCP 새로운 클라이언트 접속: {tcpSession.Socket.RemoteEndPoint}");
+                else if (session is UDPSession udpSession)
+                    Debug.Log($"UDP 새로운 클라이언트 접속: {udpSession.EndPoint}");
+            }
+
+            void OnReceivePacket(GyuNet net, Session session, Packet packet)
+            {
+                switch (packet.Header)
+                {
+                    case PacketHeader.Ping:
+                        // 플레이어 접속. 플레이어 ID 부여 후 로비로 전환
+                    {
+                        var pongPacket = new Packet();
+                        pongPacket.Serialize(session.ID);
+                        pongPacket.SetHeader(PacketHeader.Pong);
+                        net.StartSend(session, pongPacket);
+                    }
+                        break;
+                    case PacketHeader.RequestRoomJoin:
+                        // 플레이어 방 입장.
+                    {
+                        Room joinRoom = null;
+                        foreach (var room in Rooms)
+                        {
+                            lock (room.Sessions)
+                            {
+                                if (room.Sessions.Count < 4)
+                                {
+                                    joinRoom = room;
+                                    break;
+                                }
+                            }
+                        }
+                        if (joinRoom == null)
+                        {
+                            joinRoom = new Room();
+                            Rooms.Add(joinRoom);
+                        }
+                        Packet joinPacket = new Packet();
+                        lock (joinRoom.Sessions)
+                        {
+                            joinRoom.Sessions.Add(session);
+                            SessionRoomPair.TryAdd(session.ID, joinRoom);
+                            
+                            joinPacket.Serialize(joinRoom.Sessions.Count);
+                            foreach (var roomSession in joinRoom.Sessions)
+                            {
+                                joinPacket.Serialize(roomSession.ID);
+                            }
+                        }
+                        joinPacket.SetHeader(PacketHeader.RoomJoin);
+                        net.StartSend(session, joinPacket);
+                    }
+                        break;
+                    case PacketHeader.RequestRoomLeave:
+                        // 플레이어 방 퇴장.
+                    {
+                        var sessionId = session.ID;
+                        if (SessionRoomPair.TryGetValue(sessionId, out var room))
+                        {
+                            lock (room.Sessions)
+                            {
+                                room.Sessions.Remove(session);
+                                SessionRoomPair.TryRemove(sessionId, out _);
+                            }
+                        }
+
+                        Packet leavePacket = new Packet();
+                        leavePacket.SetHeader(PacketHeader.RoomLeave);
+                        net.StartSend(session, leavePacket);
+                    }
+                        break;
+                    case PacketHeader.RequestObjectSpawn:
+                        // 네트워크 오브젝트 생성.
+                    {
+                        lock (networkIDs)
+                            while (networkIDs.Contains(networkID))
+                                unchecked
+                                {
+                                    networkID++;
+                                }
+                        var spawnPacket = new Packet();
+                        spawnPacket.Serialize(networkID);
+                        spawnPacket.Serialize(packet.DeserializeString());
+                        spawnPacket.Serialize(packet.DeserializeVector3());
+                        spawnPacket.Serialize(packet.DeserializeVector3());
+                        spawnPacket.Serialize(packet.DeserializeInt());
+                        spawnPacket.SetHeader(PacketHeader.ObjectSpawn);
+                        net.StartSend(spawnPacket);
+                    }
+                        break;
+                    case PacketHeader.RequestObjectSync:
+                        // 네트워크 오브젝트 동기화
+                    {
+                        var syncPacket = new Packet();
+                        syncPacket.CopyBuffer(packet.Buffer, Define.HEADER_SIZE, packet.WriteOffset - Define.HEADER_SIZE);
+                        syncPacket.SetHeader(PacketHeader.ObjectSync);
+                        net.StartSend(syncPacket);
+                    }
+                        break;
+                    case PacketHeader.RequestObjectDespawn:
+                        // 네트워크 오브젝트 파괴
+                    {
+                        var despawnPacket = new Packet();
+                        despawnPacket.Serialize(packet.DeserializeInt());
+                        despawnPacket.SetHeader(PacketHeader.ObjectDespawn);
+                        net.StartSend(despawnPacket);
+                    }
+                        break;
+                    case PacketHeader.Chat:
+                        // 채팅.
+                    {
+                        var sessionId = session.ID;
+                        if (SessionRoomPair.TryGetValue(sessionId, out var room))
+                        {
+                            lock (room.Sessions)
+                            {
+                                foreach (var roomSession in room.Sessions)
+                                {
+                                    var chatPacket = new Packet();
+                                    chatPacket.Serialize(session.ID);
+                                    chatPacket.Serialize(packet.DeserializeString());
+                                    chatPacket.SetHeader(PacketHeader.Chat);
+                                    net.StartSend(roomSession, chatPacket);
+                                }
+                            }
+                        }
+                    }
+                        break;
+                    case PacketHeader.Rpc:
+                        // Rpc.
+                        break;
+                }
+            }
+
+            void OnDisconnect(GyuNet net, Session session)
+            {
+                var tcpSession = session as TCPSession;
+                Debug.Log($"클라이언트 접속 종료: {tcpSession?.Socket.RemoteEndPoint}");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            TcpGyuNet tcpGyuNet = new TcpGyuNet();
+            Unity.UnityGyuNet unityGyuNet = new Unity.UnityGyuNet();
+            unityGyuNet.Start();
 
-            tcpGyuNet.onAccepted += session =>
-            {
-                var tcpSession = session as TCPSession;
-                Debug.Log($"새로운 클라이언트 접속: {tcpSession?.Socket.RemoteEndPoint}");
-            };
-
-            tcpGyuNet.onReceivedPacket += (session, packet) =>
-            {
-                if (packet.Header == PacketHeader.PING)
-                {
-                    Debug.Log(packet.DeserializeBool());
-                    Debug.Log(packet.DeserializeInt());
-                    Debug.Log(packet.DeserializeInt());
-                    Debug.Log(packet.DeserializeString());
-                    Debug.Log($"Read Offset: {packet.ReadOffset} | Write Offset: {packet.WriteOffset}");
-                }
-            };
-
-            tcpGyuNet.onDisconnected += session =>
-            {
-                var tcpSession = session as TCPSession;
-                Debug.Log($"클라이언트 접속 종료: {tcpSession?.Socket.RemoteEndPoint}");
-            };
-            
-            tcpGyuNet.Start();
-            
             Debug.Log("서버 시작. esc 혹은 q를 눌러 종료");
             Debug.Log($">> {GyuNetUtility.GetIP()}:{Define.TCP_PORT}");
-            
-            while (tcpGyuNet.IsRunning)
+
+
+            while (unityGyuNet.IsRunning)
             {
                 if (Debug.CheckKey(ConsoleKey.Escape))
                 {
-                    tcpGyuNet.Stop();
+                    unityGyuNet.Stop();
                 }
             }
-            
+
             Debug.Log("서버 종료. 아무 키나 눌러 종료하세요.");
             Console.ReadKey();
         }
---
+++ GyuNet/UdpGyuNet.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GyuNet
+{
+    public class UdpGyuNet : GyuNet
+    {
+        protected readonly ConcurrentDictionary<int, Session> SessionEndPointDictionary = new ConcurrentDictionary<int, Session>();
+        
+        #region Public Method
+
+        public override void Start()
+        {
+            base.Start();
+
+            InitListenSocket();
+        }
+
+        #endregion
+
+        #region Private Method
+
+        private void InitListenSocket()
+        {
+            try
+            {
+                ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                ServerSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                ServerSocket.Bind(new IPEndPoint(IPAddress.Any, Define.UDP_PORT));
+                
+                var eventArgs = GyuNetPool.EventArgs.Pop();
+                StartReceive(eventArgs);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("UDP Socket 예외 발생");
+                Debug.LogError(e);
+                Stop();
+            }
+        }
+
+        public override void StartSend(Session session, Packet packet)
+        {
+            if (session is UDPSession udpSession)
+            {
+                var eventArgs = GyuNetPool.EventArgs.Pop();
+                eventArgs.RemoteEndPoint = udpSession.EndPoint;
+                eventArgs.UserToken = packet;
+                eventArgs.SetBuffer(packet.Buffer, 0, packet.WriteOffset);
+                if (!ServerSocket.SendToAsync(eventArgs))
+                {
+                    EventArgsOnCompleted(null, eventArgs);
+                }
+            }
+        }
+
+        protected override void StartReceive(SocketAsyncEventArgs e)
+        {
+            if (IsRunning == false)
+                return;
+
+            if (e.Buffer == null)
+            {
+                var buffer = GyuNetPool.Memories.Pop();
+                if (buffer != null)
+                    e.SetBuffer(buffer, 0, buffer.Length);
+                else
+                {
+                    Debug.LogError("메모리 풀에서 메모리를 가져오지 못했습니다.");
+                    return;
+                }
+            }
+            e.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            if (!ServerSocket.ReceiveMessageFromAsync(e))
+            {
+                EventArgsOnCompleted(null, e);
+            }
+        }
+        
+        protected override void OnAccept(SocketAsyncEventArgs e)
+        {
+            if (IsRunning == false)
+                return;
+            if (e.SocketError != SocketError.Success)
+            {
+                Debug.LogError("OnAccept Error");
+                return;
+            }
+            
+            var session = UDPSession.Pool.Pop();
+            while (ConnectedSessions.ContainsKey(sessionID))
+                unchecked
+                {
+                    sessionID++;
+                }
+            session.ID = sessionID;
+            session.EndPoint = e.RemoteEndPoint;
+
+            e.UserToken = session;
+            SessionEndPointDictionary.TryAdd(session.EndPoint.ToString().GetHashCode(), session);
+            ConnectedSessions.TryAdd(session.ID, session);
+            base.OnAccept(e);
+        }
+
+        protected override void OnSend(SocketAsyncEventArgs e)
+        {
+            if (IsRunning == false)
+                return;
+
+            if (e.SocketError != SocketError.Success)
+            {
+                Debug.LogError("OnSend Error");
+                return;
+            }
+            if (e.UserToken is Packet packet)
+                Packet.Pool.Push(packet);
+            // Send에 사용되는 버퍼는 메모리 풀에서 가져온게 아니라 Packet 버퍼를 가져온 것이다.
+            // 미리 비워두자.
+            e.SetBuffer(null, 0, 0);
+            GyuNetPool.EventArgs.Push(e);
+        }
+
+        protected override void OnReceive(SocketAsyncEventArgs e)
+        {
+            if (IsRunning == false)
+                return;
+            
+            if (e.SocketError == SocketError.Success && e.BytesTransferred > 0)
+            {
+                var hashCode = e.RemoteEndPoint.ToString().GetHashCode();
+                
+                if (!SessionEndPointDictionary.ContainsKey(hashCode))
+                {
+                    OnAccept(e);
+                }
+                
+                if (SessionEndPointDictionary.TryGetValue(hashCode, out var session))
+                {
+                    session.ReceiveData(e.Buffer, e.BytesTransferred);
+                    while (session.ReceivedPacketQueue.TryDequeue(out var rPacket))
+                    {
+                        OnReceivedPacket?.Invoke(this, session, rPacket);
+                    }
+                }
+            }
+        }
+
+        protected override void OnDisconnect(SocketAsyncEventArgs e)
+        {
+            if (IsRunning == false)
+                return;
+            base.OnDisconnect(e);
+            var session = e.UserToken as UDPSession;
+            UDPSession.Pool.Push(session);
+            GyuNetPool.EventArgs.Push(e);
+        }
+
+        #endregion
+    }
+}
